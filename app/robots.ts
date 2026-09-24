81a46889b7e7cdcb29f514b63618df9c652a2aa0
Ali Sadique <sadiquealic80@gmail.com>
2026-09-24 23:48:04 +0530

    Update domain in sitemap and robots

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://rukun-dhijaj.vercel.app/sitemap.xml",
-    host: "https://rukun-dhijaj.vercel.app",
+    sitemap: "https://rukundhijaj.com/sitemap.xml",
+    host: "https://rukundhijaj.com",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://rukun-dhijaj.vercel.app";
+  const baseUrl = "https://rukundhijaj.com";
 
   return [
     {
